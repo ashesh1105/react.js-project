2095998c8d77cb29fd13596399833ccefa7bda1e
asheshGe <ashesh.singh@ge.com>
2019-02-20 07:38:38 -0800

    Added Babel Transformer Project

---
+++ 12.projects_jsx_transformer/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BabelTransformer from './components/babel-transformer';
+
+
+
+
+ReactDOM.render(
+  <BabelTransformer />,
+  document.getElementById('app')
+);
