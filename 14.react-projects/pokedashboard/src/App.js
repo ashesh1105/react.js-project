0eda13bd0bc2cb7c3a3ed5b0b3490734bdbbb394
ashesh1105 <ashesh1105@gmail.com>
2019-08-11 15:18:12 -0700

    added pokedashboard project

--- 14.react-projects/pokedashboard/src/App.js
+++ 14.react-projects/pokedashboard/src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'whatwg-fetch';
+import PokeList from './components/PokeList'
+import { Col } from 'react-bootstrap/'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemon: []
+    };
+    this.loadPokemon = this.loadPokemon.bind(this);
+  }
+
+  loadPokemon(url) {
+    // fetch returns a promise: https://www.npmjs.com/package/whatwg-fetch
+    fetch(url)
+      .then(response => {
+        return response.json()
+      })
+      .then(json => {
+        console.log(json);
+        this.setState({
+          pokemon: json.game_indices
+        });
+        console.log(this.state)
+      }).catch(err => {
+        console.log(err);
+      });
+  }
+
+  componentDidMount() {
+    this.loadPokemon(`${this.props.baseUrl}/pokemon/1`);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React jee!</h2>
+        </div>
+        <Col sm={8} md={10} smoffset={2} mdoffset={1}>
+          <PokeList listOfPokemon={this.state.pokemon} />
+        </Col>
+      </div>
+    );
+  }
 }
+  
 
 export default App;
